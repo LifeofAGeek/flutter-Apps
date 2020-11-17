58df5d35f2c58b9bdd159dc7c687e7f7ac2d4b27
LifeofAGeek <sethi8001@gmail.com>
2020-11-17 21:09:43 +0530

    made responsive

--- expense_manager/lib/main.dart
+++ expense_manager/lib/main.dart
@@ -83,22 +83,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    final appBar = AppBar(
         title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.add),
               onPressed: () => _startAddNewTransaction(context)),
         ],
-      ),
+      );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTx),
-            TransactionList(_userTransactions, _deleteTx),
+            Container(height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top)*0.4, child: Chart(_recentTx),),
+            Container(height: (MediaQuery.of(context).size.height - appBar.preferredSize.height)*0.6, child: TransactionList(_userTransactions, _deleteTx)),
           ],
         ),
       ),
